a283cf98fac2b2f134fc655690965803c3ccb425
Sagisak <leonardo.tyoes@gmail.com>
2024-06-19 00:38:52 +0700

    finally adding the hardwork, changing everything

--- boot/boot.s
+++ boot/boot.s
@@ -1,14 +1,9 @@
-/* Assemble it using this command:i686-elf-as boot.s -o boot.o */
 /* Declare constants for the multiboot header. */
 .set ALIGN,    1<<0             /* align loaded modules on page boundaries */
 .set MEMINFO,  1<<1             /* provide memory map */
 .set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
 .set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
 .set CHECKSUM, -(MAGIC + FLAGS) /* checksum of above, to prove we are multiboot */
-
-/* Declare the framebuffer flag */
-.set FRAMEBUFFER, 1<<12         /* request framebuffer from the bootloader */
-
 
 /* 
 Declare a multiboot header that marks the program as a kernel. These are magic
@@ -50,52 +45,65 @@ doesn't make sense to return from this function as the bootloader is gone.
 .global _start
 .type _start, @function
 _start:
-    /* Set up stack */
-    mov $stack_top, %esp
+	/*
+	The bootloader has loaded us into 32-bit protected mode on a x86
+	machine. Interrupts are disabled. Paging is disabled. The processor
+	state is as defined in the multiboot standard. The kernel has full
+	control of the CPU. The kernel can only make use of hardware features
+	and any code it provides as part of itself. There's no printf
+	function, unless the kernel provides its own <stdio.h> header and a
+	printf implementation. There are no security restrictions, no
+	safeguards, no debugging mechanisms, only what the kernel provides
+	itself. It has absolute and complete power over the
+	machine.
+	*/
 
-    /* Initialize crucial processor state if necessary */
+	/*
+	To set up a stack, we set the esp register to point to the top of the
+	stack (as it grows downwards on x86 systems). This is necessarily done
+	in assembly as languages such as C cannot function without a stack.
+	*/
+	mov $stack_top, %esp
 
-    /* Load VGA font from BIOS into buffer */
-    push %ds
-    push %es
+	/*
+	This is a good place to initialize crucial processor state before the
+	high-level kernel is entered. It's best to minimize the early
+	environment where crucial features are offline. Note that the
+	processor is not fully initialized yet: Features such as floating
+	point instructions and instruction set extensions are not initialized
+	yet. The GDT should be loaded here. Paging should be enabled here.
+	C++ features such as global constructors and exceptions will require
+	runtime support to work as well.
+	*/
 
-    /* Set ES:DI to point to our buffer (adjust address as needed) */
-    mov $buffer, %eax       /* Address of your buffer */
-    mov %eax, %es
-    xor %edi, %edi          /* EDI = 0 */
+	/*
+	Enter the high-level kernel. The ABI requires the stack is 16-byte
+	aligned at the time of the call instruction (which afterwards pushes
+	the return pointer of size 4 bytes). The stack was originally 16-byte
+	aligned above and we've pushed a multiple of 16 bytes to the
+	stack since (pushed 0 bytes so far), so the alignment has thus been
+	preserved and the call is well defined.
+	*/
+	call kernel_main
 
-    /* Ask BIOS to return VGA bitmap fonts */
-    mov $0x1130, %ax        /* BIOS video services: get font info */
-    mov $0x06, %bh          /* Font height (6x8 pixels) */
-    int $0x10
+	/*
+	If the system has nothing more to do, put the computer into an
+	infinite loop. To do that:
+	1) Disable interrupts with cli (clear interrupt enable in eflags).
+	   They are already disabled by the bootloader, so this is not needed.
+	   Mind that you might later enable interrupts and return from
+	   kernel_main (which is sort of nonsensical to do).
+	2) Wait for the next interrupt to arrive with hlt (halt instruction).
+	   Since they are disabled, this will lock up the computer.
+	3) Jump to the hlt instruction if it ever wakes up due to a
+	   non-maskable interrupt occurring or due to system management mode.
+	*/
+	cli
+1:	hlt
+	jmp 1b
 
-    /* Copy charmap to our buffer */
-    push %es
-    pop %ds
-    mov %ebp, %esi           // BP contains the address returned by BIOS
-    mov $256*16, %ecx        // Number of bytes to copy (256 glyphs * 16 bytes each)
-    rep movsb                // Move data from DS:SI to ES:DI
-
-
-    pop %es
-    pop %ds
-
-    /* Call kernel_main or perform other initialization */
-
-    /* Enter high-level kernel or other operations */
-
-    /* Infinite loop */
-    cli
-1:  hlt
-    jmp 1b
-
+/*
+Set the size of the _start symbol to the current location '.' minus its start.
+This is useful when debugging or when you implement call tracing.
+*/
 .size _start, . - _start
-
-.section .bss
-.align 16
-
-.section .data
-.balign 4
-buffer:
-    .skip 4096 /* 4 KiB buffer for font data */
-
