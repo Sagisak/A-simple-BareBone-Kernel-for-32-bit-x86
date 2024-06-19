5fe731f248ae73445705abdf597837464d6b5686
Sagisak <leonardo.tyoes@gmail.com>
2024-06-19 11:58:47 +0700

    adding the newline function plus changin the myos.iso

---
+++ UPDATE.md
@@ -0,0 +1,5 @@
+## UPDATE LOG FOR EACH VERSION
+
+# myos-1.2
+
+So the previous version the newline does not work, in this version, the terminal_putchar has been modified so it can take newline
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -84,13 +84,18 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 }
 
 void terminal_putchar(char c) 
-{
+{	if( c != '\n') {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
 			terminal_row = 0;
 	}
+	} else {
+	terminal_column = 0;
+	++terminal_row;
+	}
+	
 }
 
 void terminal_write(const char* data, size_t size) 
@@ -111,4 +116,5 @@ void kernel_main(void)
 
 	/* Newline support is left as an exercise. */
 	terminal_writestring("Hello, kernel World!\n");
-}
+	terminal_writestring("This is the newline!");
+} 
